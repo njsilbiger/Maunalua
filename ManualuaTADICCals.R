6da443767758b450dd701a3e168285dcc5d1b47b
Nyssa Silbiger <nyssa.silbiger@csun.edu>
2019-09-23 16:36:01 -0700

    Added mapping script and started looking at feedbacks

--- ManualuaTADICCals.R
+++ ManualuaTADICCals.R
@@ -29,70 +29,70 @@ Cdata[,c("CO2","HCO3","CO3","DIC","OmegaArag","OmegaCalcite","pCO2","fCO2")]<-
 BP.end.DIC<-3038
 BP.end.TA<-2945
 BP.end.Sal<-4.9
+BP.end.PO<-3.7
+BP.end.NN<-163
+BP.end.Si<-740
+
 #wailupe
 W.end.DIC<-1780
 W.end.TA<-1743
 W.end.Sal<-2
+W.end.PO<-1.7
+W.end.NN<-71
+W.end.Si<-810
 
 #hot end point from 05/24/2015
 Hot.Sal<- 35.1961
 Hot.pH<-8.069
 Hot.TA<-2324
 Hot.temp<-24.4490
+Hot.PO<-0.11
+Hot.NN<-0.01
+Hot.Si<-1.17
+
 
 Hot.CO2<-carb(flag=8, Hot.pH, Hot.TA/1000000, S=Hot.Sal, T=Hot.temp, Patm=1, P=0, Pt=0, Sit=0,
           k1k2="x", kf="x", ks="d", pHscale="T", b="u74", gas="potential")
 
 Hot.CO2[,c("CO2","HCO3","CO3","DIC","ALK")]<-Hot.CO2[,c("CO2","HCO3","CO3","DIC","ALK")]*1000000
 
-#calculate a mixing line for TA
-#Black point
-par(mfrow=c(1,2))
-plot(c(BP.end.Sal,Hot.Sal), c(BP.end.TA,Hot.TA), type='l', col = 'red', main = 'Black Point', xlab='Salinity', ylab='TA', xlim = c(0,36), ylim = c(2100,3000))
-points(Cdata$Salinity[Cdata$Site=='BP' & Cdata$Tide=='L1' & Cdata$Season=='SPRING'],Cdata$TA[Cdata$Site=='BP' & Cdata$Tide=='L1' & Cdata$Season=='SPRING'])
-points(Cdata$Salinity[Cdata$Site=='BP' & Cdata$Tide=='L2' & Cdata$Season=='SPRING'],Cdata$TA[Cdata$Site=='BP' & Cdata$Tide=='L2' & Cdata$Season=='SPRING'], pch=19)
-points(Cdata$Salinity[Cdata$Site=='BP' & Cdata$Tide=='H1' & Cdata$Season=='SPRING'],Cdata$TA[Cdata$Site=='BP' & Cdata$Tide=='H1' & Cdata$Season=='SPRING'], pch=0)
-points(Cdata$Salinity[Cdata$Site=='BP' & Cdata$Tide=='H2' & Cdata$Season=='SPRING'],Cdata$TA[Cdata$Site=='BP' & Cdata$Tide=='H2' & Cdata$Season=='SPRING'], pch=15)
-#wailupe
-plot(c(W.end.Sal,Hot.Sal), c(W.end.TA,Hot.TA), col='blue', main = 'Wailupe', type='l', xlab='Salinity', ylab='TA')
-points(Cdata$Salinity[Cdata$Site=='W' & Cdata$Tide=='L1' & Cdata$Season=='SPRING'],Cdata$TA[Cdata$Site=='W' & Cdata$Tide=='L1' & Cdata$Season=='SPRING'])
-points(Cdata$Salinity[Cdata$Site=='W' & Cdata$Tide=='L2' & Cdata$Season=='SPRING'],Cdata$TA[Cdata$Site=='W' & Cdata$Tide=='L2' & Cdata$Season=='SPRING'], pch=19)
-points(Cdata$Salinity[Cdata$Site=='W' & Cdata$Tide=='H1' & Cdata$Season=='SPRING'],Cdata$TA[Cdata$Site=='W' & Cdata$Tide=='H1' & Cdata$Season=='SPRING'], pch=0)
-points(Cdata$Salinity[Cdata$Site=='W' & Cdata$Tide=='H2' & Cdata$Season=='SPRING'],Cdata$TA[Cdata$Site=='W' & Cdata$Tide=='H2' & Cdata$Season=='SPRING'], pch=15)
-
-#DIC
-#Black point
-par(mfrow=c(1,2))
-plot(c(BP.end.Sal,Hot.Sal), c(BP.end.DIC, Hot.CO2$DIC), type='l', col = 'red', main = 'Black Point', xlab='Salinity', ylab='DIC', xlim = c(0,36), ylim = c(1800,3200))
-points(Cdata$Salinity[Cdata$Site=='BP' & Cdata$Tide=='L1' & Cdata$Season=='SPRING'],Cdata$DIC[Cdata$Site=='BP' & Cdata$Tide=='L1' & Cdata$Season=='SPRING'])
-points(Cdata$Salinity[Cdata$Site=='BP' & Cdata$Tide=='L2' & Cdata$Season=='SPRING'],Cdata$DIC[Cdata$Site=='BP' & Cdata$Tide=='L2' & Cdata$Season=='SPRING'], pch=19)
-points(Cdata$Salinity[Cdata$Site=='BP' & Cdata$Tide=='H1' & Cdata$Season=='SPRING'],Cdata$DIC[Cdata$Site=='BP' & Cdata$Tide=='H1' & Cdata$Season=='SPRING'], pch=0)
-points(Cdata$Salinity[Cdata$Site=='BP' & Cdata$Tide=='H2' & Cdata$Season=='SPRING'],Cdata$DIC[Cdata$Site=='BP' & Cdata$Tide=='H2' & Cdata$Season=='SPRING'], pch=15)
-#wailupe
-plot(c(W.end.Sal,Hot.Sal), c(W.end.DIC,Hot.CO2$DIC), col='blue', main = 'Wailupe', type='l', xlab='Salinity', ylab='DIC')
-points(Cdata$Salinity[Cdata$Site=='W' & Cdata$Tide=='L1' & Cdata$Season=='SPRING'],Cdata$DIC[Cdata$Site=='W' & Cdata$Tide=='L1' & Cdata$Season=='SPRING'])
-points(Cdata$Salinity[Cdata$Site=='W' & Cdata$Tide=='L2' & Cdata$Season=='SPRING'],Cdata$DIC[Cdata$Site=='W' & Cdata$Tide=='L2' & Cdata$Season=='SPRING'], pch=19)
-points(Cdata$Salinity[Cdata$Site=='W' & Cdata$Tide=='H1' & Cdata$Season=='SPRING'],Cdata$DIC[Cdata$Site=='W' & Cdata$Tide=='H1' & Cdata$Season=='SPRING'], pch=0)
-points(Cdata$Salinity[Cdata$Site=='W' & Cdata$Tide=='H2' & Cdata$Season=='SPRING'],Cdata$DIC[Cdata$Site=='W' & Cdata$Tide=='H2' & Cdata$Season=='SPRING'], pch=15)
-
 #create the regression
 
-#black point
+#
+#TA
 BP.model<-lm(c(BP.end.TA,Hot.TA)~c(BP.end.Sal,Hot.Sal))
 W.model<-lm(c(W.end.TA,Hot.TA)~c(W.end.Sal,Hot.Sal))
 #DIC
 BP.model.DIC<-lm(c(BP.end.DIC,Hot.CO2$DIC)~c(BP.end.Sal,Hot.Sal))
 W.model.DIC<-lm(c(W.end.DIC,Hot.CO2$DIC)~c(W.end.Sal,Hot.Sal))
+#NN
+BP.model.NN<-lm(c(BP.end.NN,Hot.NN)~c(BP.end.Sal,Hot.Sal))
+W.model.NN<-lm(c(W.end.NN,Hot.NN)~c(W.end.Sal,Hot.Sal))
+#PO
+BP.model.PO<-lm(c(BP.end.PO,Hot.PO)~c(BP.end.Sal,Hot.Sal))
+W.model.PO<-lm(c(W.end.PO,Hot.PO)~c(W.end.Sal,Hot.Sal))
+
 
 #predicted TA based on mixing line
 BP.TA.Pred<-Cdata$Salinity[Cdata$Site=='BP']*BP.model$coefficients[2]+BP.model$coefficients[1]
 W.TA.Pred<-Cdata$Salinity[Cdata$Site=='W']*W.model$coefficients[2]+W.model$coefficients[1]
-
+Cdata$TA.pred<-c(W.TA.Pred, BP.TA.Pred)
 
 #predicted DIC based on mixing line
 BP.DIC.Pred<-Cdata$Salinity[Cdata$Site=='BP']*BP.model.DIC$coefficients[2]+BP.model.DIC$coefficients[1]
 W.DIC.Pred<-Cdata$Salinity[Cdata$Site=='W']*W.model.DIC$coefficients[2]+W.model.DIC$coefficients[1]
+Cdata$DIC.pred<-c(W.DIC.Pred, BP.DIC.Pred)
 
+#predicted NN based on mixing line
+BP.NN.Pred<-Cdata$Salinity[Cdata$Site=='BP']*BP.model.NN$coefficients[2]+BP.model.NN$coefficients[1]
+W.NN.Pred<-Cdata$Salinity[Cdata$Site=='W']*W.model.NN$coefficients[2]+W.model.NN$coefficients[1]
+Cdata$NN.pred<-c(W.NN.Pred, BP.NN.Pred)
+
+
+#predicted NN based on mixing line
+BP.PO.Pred<-Cdata$Salinity[Cdata$Site=='BP']*BP.model.PO$coefficients[2]+BP.model.PO$coefficients[1]
+W.PO.Pred<-Cdata$Salinity[Cdata$Site=='W']*W.model.PO$coefficients[2]+W.model.PO$coefficients[1]
+Cdata$PO.pred<-c(W.PO.Pred, BP.PO.Pred)
 
 #calculate difference between mixing line and actual TA
 BP.diff<- BP.TA.Pred-Cdata$TA[Cdata$Site=='BP'] #positive values are calcification and negative are dissolution
@@ -106,6 +106,45 @@ W.diff.DIC<- W.DIC.Pred-Cdata$DIC[Cdata$Site=='W'] #positive values are calcific
 DIC.diff<-c(W.diff.DIC,BP.diff.DIC)
 Cdata$DIC.diff<-DIC.diff
 
+#calculate difference between mixing line and actual NN
+BP.diff.NN<- BP.NN.Pred-Cdata$NN[Cdata$Site=='BP'] #positive values are calcification and negative are dissolution
+W.diff.NN<- W.NN.Pred-Cdata$NN[Cdata$Site=='W'] #positive values are calcification and negative are dissolution
+NN.diff<-c(W.diff.NN,BP.diff.NN)
+Cdata$NN.diff<-NN.diff
+
+#calculate difference between mixing line and actual PO
+BP.diff.PO<- BP.PO.Pred-Cdata$Phosphate[Cdata$Site=='BP'] #positive values are calcification and negative are dissolution
+W.diff.PO<- W.PO.Pred-Cdata$Phosphate[Cdata$Site=='W'] #positive values are calcification and negative are dissolution
+PO.diff<-c(W.diff.PO,BP.diff.PO)
+Cdata$PO.diff<-PO.diff
+
+#### plot raw data and mixing line
+
+ggplot(Cdata, aes(group = Site))+
+  geom_point(aes(x = Salinity, y = TA))+
+  geom_line(aes(x=Salinity, y = TA.pred), col = 'blue')+
+  ggtitle('Total Alkalinity')+
+  facet_wrap(~Site)
+
+
+ggplot(Cdata, aes(group = Site))+
+  geom_point(aes(x = Salinity, y = DIC))+
+  geom_line(aes(x=Salinity, y = DIC.pred), col = 'blue')+
+  ggtitle('Dissolved Inorganic Carbon')+
+  facet_wrap(~Site)
+
+ggplot(Cdata, aes(group = Site))+
+  geom_point(aes(x = Salinity, y = NN))+
+  geom_line(aes(x=Salinity, y = NN.pred), col = 'blue')+
+  ggtitle('N+N')+
+  facet_wrap(~Site)
+
+
+ggplot(Cdata, aes(group = Site))+
+  geom_point(aes(x = Salinity, y = Phosphate))+
+  geom_line(aes(x=Salinity, y = PO.pred), col = 'blue')+
+  ggtitle('PO')+
+  facet_wrap(~Site)
 
 ####################Anaysis########################### 
 # run anova to for Wailupe
@@ -206,3 +245,67 @@ AllZone<-plot_grid(BP.plot.zone, W.plot.zone, labels=c('A', 'B'), nrow = 2)
 #save the output in the output folder
 ggsave(filename = 'Output/PlotsbyTide.pdf', plot = AllTide,device = 'pdf', width = 10, height = 8)
 ggsave(filename = 'Output/PlotsbyZone.pdf', plot = AllZone,device = 'pdf', width = 8, height = 8)
+
+
+
+## Look at relationship between feedbacks and salinity versus deltas
+ggplot(Cdata, aes(group = Site))+
+  geom_point(aes(x = Salinity, y = pH))+
+  facet_wrap(~Site)
+
+
+Cdata2<-Cdata %>%
+  filter(TA.diff <300)
+
+ggplot(Cdata2, aes(y = TA.diff, x = pH,group = Zone))+
+  geom_point(aes( col = Zone))+
+  geom_smooth(method = "lm", aes(y = TA.diff, x = pH, group = Zone, col = Zone))+
+  facet_wrap(~Site)
+
+ggplot(Cdata2, aes(x = DIC.diff, y = pH,group = Zone))+
+  geom_point(aes( col = Zone))+
+  geom_smooth(method = "lm", aes(x = DIC.diff, y = pH, group = Zone, col = Zone))+
+  facet_wrap(~Site)
+
+
+a<-lm(pH~DIC.diff*Salinity, data = Cdata, subset = Cdata$Site=='W')
+anova(a)
+
+Cdata %>%
+#  filter(Salinity<33)%>%
+ggplot(aes(x = DIC.diff, y = pH, col = Salinity, group = Site))+
+  geom_point(aes(shape = Tide, size = 1))+
+  scale_colour_gradient2(low = "red", high = "blue3", space = "Lab",midpoint = 33,
+                         na.value = "grey50", guide = "colourbar", aesthetics = "colour")+
+  facet_wrap(~Site)+
+  theme_bw()
+
+Cdata %>%
+  #  filter(Salinity<33)%>%
+  ggplot(aes(x = pH, y = TA.diff/2, col = Salinity, group = Site))+
+  geom_point(aes(shape = Tide, size = 1))+
+  scale_colour_gradient2(low = "red", high = "blue3", space = "Lab",midpoint = 33,
+                         na.value = "grey50", guide = "colourbar", aesthetics = "colour")+
+  facet_wrap(~Site)+
+  theme_bw()
+
+
+#NN difference versus salinity
+Cdata %>%
+  #  filter(Salinity<33)%>%
+  ggplot(aes(y = NN.diff, x = Salinity, col = Salinity, group = Site))+
+  geom_point(aes(size = 1))+
+  scale_colour_gradient2(low = "red", high = "blue3", space = "Lab",midpoint = 33,
+                         na.value = "grey50", guide = "colourbar", aesthetics = "colour")+
+  facet_wrap(~Site)+
+  theme_bw()
+
+#PO difference versus salinity
+Cdata %>%
+  #  filter(Salinity<33)%>%
+  ggplot(aes(y = PO.diff, x = Salinity, col = Salinity, group = Site))+
+  geom_point(aes(size = 1))+
+  scale_colour_gradient2(low = "grey", high = "blue", mid = 'lightblue', midpoint = 31, space = "Lab", limits = c(25,35),
+                         breaks = c(25,27,29,31,33,35))+
+  facet_wrap(~Site)+
+  theme_bw()
---
+++ MaunaluaModelScript.R
@@ -0,0 +1,180 @@
+
+#calculate residuls for TA and DIC analysis
+#libraries
+library(seacarb)
+library(lme4)
+library(lmerTest)
+library(cowplot)
+library(tidyverse)
+library(scales)
+library(sf)
+library(gstat)
+library(stars)
+
+#load data
+Cdata<-read.csv('chemicaldata_maunalua.csv')
+#remove rows with NAs
+Cdata<-Cdata[complete.cases(Cdata),]
+
+#calculate rest of carbonate params
+CO2<-carb(flag=8, Cdata$pH, Cdata$TA/1000000, S=Cdata$Salinity, T=Cdata$Temp_in, Patm=1, P=0, Pt=0, Sit=0,
+          k1k2="x", kf="x", ks="d", pHscale="T", b="u74", gas="potential")
+#TA is divided by 1000 because all calculations are in mol/kg in the seacarb package
+
+#convert CO2, HCO3, CO3, DIC, and Alk back to micromol for easier interpretation
+CO2[,c("CO2","HCO3","CO3","DIC","ALK")]<-CO2[,c("CO2","HCO3","CO3","DIC","ALK")]*1000000
+
+Cdata[,c("CO2","HCO3","CO3","DIC","OmegaArag","OmegaCalcite","pCO2","fCO2")]<-
+  CO2[,c("CO2","HCO3","CO3","DIC","OmegaAragonite","OmegaCalcite","pCO2","fCO2")]
+
+
+#endmembers for Christina
+#black point
+BP.end.DIC<-3038
+BP.end.TA<-2945
+BP.end.Sal<-4.9
+#wailupe
+W.end.DIC<-1780
+W.end.TA<-1743
+W.end.Sal<-2
+
+#hot end point from 05/24/2015
+Hot.Sal<- 35.1961
+Hot.pH<-8.069
+Hot.TA<-2324
+Hot.temp<-24.4490
+
+Hot.CO2<-carb(flag=8, Hot.pH, Hot.TA/1000000, S=Hot.Sal, T=Hot.temp, Patm=1, P=0, Pt=0, Sit=0,
+              k1k2="x", kf="x", ks="d", pHscale="T", b="u74", gas="potential")
+
+Hot.CO2[,c("CO2","HCO3","CO3","DIC","ALK")]<-Hot.CO2[,c("CO2","HCO3","CO3","DIC","ALK")]*1000000
+
+#black point
+BP.model<-lm(c(BP.end.TA,Hot.TA)~c(BP.end.Sal,Hot.Sal))
+W.model<-lm(c(W.end.TA,Hot.TA)~c(W.end.Sal,Hot.Sal))
+#DIC
+BP.model.DIC<-lm(c(BP.end.DIC,Hot.CO2$DIC)~c(BP.end.Sal,Hot.Sal))
+W.model.DIC<-lm(c(W.end.DIC,Hot.CO2$DIC)~c(W.end.Sal,Hot.Sal))
+
+#predicted TA based on mixing line
+BP.TA.Pred<-Cdata$Salinity[Cdata$Site=='BP']*BP.model$coefficients[2]+BP.model$coefficients[1]
+W.TA.Pred<-Cdata$Salinity[Cdata$Site=='W']*W.model$coefficients[2]+W.model$coefficients[1]
+Cdata$TA.pred<-c(W.TA.Pred, BP.TA.Pred)
+
+#predicted DIC based on mixing line
+BP.DIC.Pred<-Cdata$Salinity[Cdata$Site=='BP']*BP.model.DIC$coefficients[2]+BP.model.DIC$coefficients[1]
+W.DIC.Pred<-Cdata$Salinity[Cdata$Site=='W']*W.model.DIC$coefficients[2]+W.model.DIC$coefficients[1]
+Cdata$DIC.pred<-c(W.DIC.Pred, BP.DIC.Pred)
+
+#calculate difference between mixing line and actual TA
+BP.diff<- BP.TA.Pred-Cdata$TA[Cdata$Site=='BP'] #positive values are calcification and negative are dissolution
+W.diff<- W.TA.Pred-Cdata$TA[Cdata$Site=='W'] #positive values are calcification and negative are dissolution
+TA.diff<-c(W.diff,BP.diff)
+Cdata$TA.diff<-TA.diff
+
+#calculate difference between mixing line and actual DIC
+BP.diff.DIC<- BP.DIC.Pred-Cdata$DIC[Cdata$Site=='BP'] #positive values are calcification and negative are dissolution
+W.diff.DIC<- W.DIC.Pred-Cdata$DIC[Cdata$Site=='W'] #positive values are calcification and negative are dissolution
+DIC.diff<-c(W.diff.DIC,BP.diff.DIC)
+Cdata$DIC.diff<-DIC.diff
+
+
+#### plot raw data and mixing line
+
+ggplot(Cdata, aes(group = Site))+
+  geom_point(aes(x = Salinity, y = TA))+
+  geom_line(aes(x=Salinity, y = TA.pred), col = 'blue')+
+  ggtitle('Total Alkalinity')+
+  facet_wrap(~Site)
+
+
+ggplot(Cdata, aes(group = Site))+
+  geom_point(aes(x = Salinity, y = DIC))+
+  geom_line(aes(x=Salinity, y = DIC.pred), col = 'blue')+
+  ggtitle('Dissolved Inorganic Carbon')+
+  facet_wrap(~Site)
+
+
+##### Make a map of the deltas for each
+
+Cdata.BP<-Cdata %>%
+  dplyr::filter(Site =='W', Season == 'FALL', Tide == "L1")
+
+coordinates(Cdata.BP) =~ Long+Lat
+
+kriging_result = autoKrige(DIC.diff~1, Cdata.BP)
+plot(kriging_result, sp.layout = list(pts = list("sp.points", Cdata.BP)), justPosition = FALSE)
+
+## pull out the results
+coords<-as.data.frame(kriging_result$krige_output@coords)
+coords$preds<-kriging_result$krige_output@data$var1.pred
+
+ggplot(coords, aes(x = x1, y = x2, col = preds))+
+  geom_tile()+
+ # scale_colour_gradient2(low = muted("red"), mid = "white",
+ #                        high = muted("blue"), midpoint = 0, space = "Lab",
+#                         na.value = "grey50", guide = "colourbar", aesthetics = "colour")+
+  ggtitle('Low Tide Fall')
+
+
+Cdata.BP<-Cdata %>%
+  dplyr::filter(Site =='BP', Season == 'FALL', Tide == "H1")
+
+coordinates(Cdata.BP) =~ Long+Lat
+
+
+kriging_result = autoKrige(TA.diff~1, Cdata.BP)
+plot(kriging_result, sp.layout = list(pts = list("sp.points", Cdata.BP)), justPosition = TRUE)
+
+coords<-as.data.frame(kriging_result$krige_output@coords)
+coords$preds<-kriging_result$krige_output@data$var1.pred
+
+ggplot(coords, aes(x = x1, y = x2, col = preds))+
+  geom_tile()+
+  scale_colour_gradient2(low = muted("red"), mid = "white",
+                         high = muted("blue"), midpoint = 0, space = "Lab",
+                         na.value = "grey50", guide = "colourbar", aesthetics = "colour")
+
+#kriging_result$krige_output@data$var1.pred
+
+#create a variogram
+# create coordinates
+coordinates(Cdata) = ~Long+Lat
+
+v = variogram(TA.diff~1, data = Cdata)
+plot(v, plot.numbers = TRUE)
+
+v.m = fit.variogram(v, vgm(1, "Exp", 2000,1))
+plot(v, v.m, plot.numbers = TRUE)
+
+ggplot(Cdata, aes(group = Site))+
+  geom_point(aes(x = Long, y = Lat, color = TA.diff, size = 1))+
+  scale_colour_gradient2(low = "red", mid = "white",
+                         high = "blue", midpoint = 0, space = "Lab",
+                         na.value = "grey50", guide = "colourbar", aesthetics = "colour")+
+  facet_wrap(Site~Tide+Season, scales = 'free')
+
+Cdata %>%
+  filter(Site=='W')%>%
+ggplot()+
+  geom_point(aes(x = Long, y = Lat, color = DIC.diff, size = 1))+
+  scale_colour_gradient2(low = "red", mid = "white",
+                         high = "blue", midpoint = 0, space = "Lab",
+                         na.value = "grey50", guide = "colourbar", aesthetics = "colour")+
+  facet_wrap(Season~Tide)+
+  ggtitle('Wailupe')
+
+
+Cdata %>%
+  filter(Site=='BP')%>%
+  ggplot()+
+  geom_point(aes(x = Long, y = Lat, color = DIC.diff, size = 1))+
+  scale_colour_gradient2(low = "red", mid = "white",
+                         high = "blue", midpoint = 0, space = "Lab",
+                         na.value = "grey50", guide = "colourbar", aesthetics = "colour")+
+  facet_wrap(Season~Tide)+
+  ggtitle('Black Point')
+
+
+####################Anaysis########################### 
+
